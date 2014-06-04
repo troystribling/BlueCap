a53900085dd0d792a05267109244924d7b2043f8
Troy Stribling <troy.stribling@gmail.com>
2014-06-03 21:24:53 -0700

    created project

---
+++ BlueCap/BlueCap.h
@@ -0,0 +1,19 @@
+//
+//  BlueCap.h
+//  BlueCap
+//
+//  Created by Troy Stribling on 6/3/14.
+//  Copyright (c) 2014 gnos.us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BlueCap.
+FOUNDATION_EXPORT double BlueCapVersionNumber;
+
+//! Project version string for BlueCap.
+FOUNDATION_EXPORT const unsigned char BlueCapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlueCap/PublicHeader.h>
+
+
