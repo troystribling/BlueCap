cd969659a4c110d1e9079c14064d86f6bff0be46
Troy Stribling <troy.stribling@gmail.com>
2014-06-04 21:43:02 -0700

    dynamic framework works

---
+++ BlueCap/CentralManager.swift
@@ -0,0 +1,38 @@
+//
+//  CentralManager.swift
+//  BlueCap
+//
+//  Created by Troy Stribling on 6/4/14.
+//  Copyright (c) 2014 gnos.us. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+class CentralManager : NSObject, CBCentralManagerDelegate {
+    
+    // CBCentralManagerDelegate
+    func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
+    }
+    
+    func centralManager(central: CBCentralManager!, didDisconnectPeripheral peripheral: CBPeripheral!, error: NSError!) {
+    }
+    
+    func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: NSDictionary!, RSSI: NSNumber!) {
+    }
+    
+    func centralManager(central: CBCentralManager!, didFailToConnectPeripheral peripheral: CBPeripheral!, error: NSError!) {
+    }
+    
+    func centralManager(central: CBCentralManager!, didRetrieveConnectedPeripherals peripherals: AnyObject[]!) {
+    }
+    
+    func centralManager(central: CBCentralManager!, didRetrievePeripherals peripherals: AnyObject[]!) {
+    }
+    
+    func centralManager(central: CBCentralManager!, willRestoreState dict: NSDictionary!) {
+    }
+    
+    func centralManagerDidUpdateState(central: CBCentralManager!) {
+    }
+}
--- BlueCapUI/BlueCapUI/FirstViewController.swift
+++ BlueCapUI/BlueCapUI/FirstViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import BlueCap
 
 class FirstViewController: UIViewController {
                             
