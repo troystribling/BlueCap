7fc826291d40d62ab384c5cccad3bc8d29c875db
Troy Stribling <troy.stribling@gmail.com>
2014-08-03 13:14:52 -0700

    added nordic profiles

--- BlueCapKit/NordicProfiles.swift
+++ BlueCapKit/NordicProfiles.swift
@@ -15,19 +15,47 @@ public struct Nordic {
     //***************************************************************************************************
     // Nordic Device Temperature Service
     //***************************************************************************************************
-    struct DeviceTemperature {
+    struct DeviceTemperatureService {
         static let uuid = "2f0a0003-69aa-f316-3e78-4194989a6c1a"
         static let name = "Noric Device Temperature"
         struct Data {
             static let uuid = "2f0a0004-69aa-f316-3e78-4194989a6c1a"
             static let name = "Device Temperature Data"
+            struct Value : DeserializedStruct {
+                var temperatureRaw  : Int16
+                var temperature     : Double
+                static func fromRawValues(rawValues:[Int16]) -> Value? {
+                    if rawValues.count == 1 {
+                        return Value(temperatureRaw:rawValues[0], temperature:self.valueFromRaw(rawValues[0]))
+                    } else {
+                        return nil
+                    }
+                }
+                static func fromStrings(stringValues:Dictionary<String, String>) -> Value? {
+                    let temperatureRaw = BlueCap.int16ValueFromStringValue("temperatureRaw", values:stringValues)
+                    if temperatureRaw {
+                        return Value(temperatureRaw:temperatureRaw!, temperature:self.valueFromRaw(temperatureRaw!))
+                    } else {
+                        return nil
+                    }
+                }
+                static func valueFromRaw(rawValue:Int16) -> Double {
+                    return Double(rawValue)/4.0
+                }
+                var stringValues : Dictionary<String,String> {
+                    return ["temperatureRaw":"\(temperatureRaw)", "temperature":"\(temperature)"]
+                }
+                func toRawValues() -> [Int16] {
+                    return [temperatureRaw]
+                }
+            }
         }
     }
 
     //***************************************************************************************************
     // Nordic BLE Address Service
     //***************************************************************************************************
-    struct BLEAddress {
+    struct BLEAddressService {
         static let uuid = "2f0a0005-69aa-f316-3e78-4194989a6c1a"
         static let name = "Noric BLE Address"
         struct Address {
@@ -41,8 +69,12 @@ public struct Nordic {
                 var addr5 : UInt8
                 var addr6 : UInt8
                 static func fromRawValues(rawValues:[UInt8]) -> Value? {
-                    return Value(addr1:rawValues[0], addr2:rawValues[1], addr3:rawValues[2],
-                                 addr4:rawValues[3], addr5:rawValues[4], addr6:rawValues[5])
+                    if rawValues.count == 6 {
+                        return Value(addr1:rawValues[0], addr2:rawValues[1], addr3:rawValues[2],
+                                     addr4:rawValues[3], addr5:rawValues[4], addr6:rawValues[5])
+                    } else {
+                        return nil
+                    }
                 }
                 static func fromStrings(stringValues:Dictionary<String, String>) -> Value? {
                     let addr1 = BlueCap.uint8ValueFromStringValue("addr1", values:stringValues)
@@ -66,7 +98,7 @@ public struct Nordic {
                 }
             }
         }
-        struct Type {
+        struct AddressType {
             static let uuid = "2f0a0007-69aa-f316-3e78-4194989a6c1a"
             static let name = "BLE Address Type"
             enum Value : UInt8, DeserializedEnum {
@@ -111,17 +143,17 @@ public struct Nordic {
                     return ["Unknown", "Public", "RandomStatic", "RandomPrivateResolvable", "RandomPrivateUnresolvable"]
                 }
                 var stringValue : String {
-                    switch self {
-                    case .Unknown:
-                        return "Unknown"
-                    case .Public:
-                        return "Public"
-                    case .RandomStatic:
-                        return "RandomStatic"
-                    case .RandomPrivateResolvable:
-                        return "RandomPrivateResolvable"
-                    case .RandomPrivateUnresolvable:
-                        return "RandomPrivateUnresolvable"
+                switch self {
+                case .Unknown:
+                    return "Unknown"
+                case .Public:
+                    return "Public"
+                case .RandomStatic:
+                    return "RandomStatic"
+                case .RandomPrivateResolvable:
+                    return "RandomPrivateResolvable"
+                case .RandomPrivateUnresolvable:
+                    return "RandomPrivateUnresolvable"
                     }
                 }
                 func toRaw() -> UInt8 {
@@ -141,7 +173,6 @@ public struct Nordic {
             }
         }
     }
-    
 }
 
 public class NordicProfiles {
@@ -153,13 +184,32 @@ public class NordicProfiles {
         //***************************************************************************************************
         // Nordic Device Temperature Service
         //***************************************************************************************************
-        profileManager.addService(ServiceProfile(uuid:Nordic.DeviceTemperature.uuid, name:Nordic.DeviceTemperature.name){(serviceProfile:ServiceProfile) in
+        profileManager.addService(ServiceProfile(uuid:Nordic.DeviceTemperatureService.uuid, name:Nordic.DeviceTemperatureService.name){(serviceProfile) in
+            // Device Temperature Data
+            serviceProfile.addCharacteristic(StructCharacteristicProfile<Nordic.DeviceTemperatureService.Data.Value>(uuid:Nordic.DeviceTemperatureService.Data.uuid, name:Nordic.DeviceTemperatureService.Data.name)
+                {(characteristicProfile) in
+                    characteristicProfile.endianness = .Big
+                    characteristicProfile.initialValue = NSData.serializeToBigEndian(Nordic.DeviceTemperatureService.Data.Value.fromRawValues([100])?.toRawValues())
+                    characteristicProfile.properties = CBCharacteristicProperties.Read | CBCharacteristicProperties.Write
+                })
         })
 
         //***************************************************************************************************
         // Nordic BLE Address Service
         //***************************************************************************************************
-        profileManager.addService(ServiceProfile(uuid:Nordic.BLEAddress.uuid, name:Nordic.BLEAddress.name){(serviceProfile:ServiceProfile) in
+        profileManager.addService(ServiceProfile(uuid:Nordic.BLEAddressService.uuid, name:Nordic.BLEAddressService.name){(serviceProfile) in
+            // BLE Address Address
+            serviceProfile.addCharacteristic(StructCharacteristicProfile<Nordic.BLEAddressService.Address.Value>(uuid:Nordic.BLEAddressService.Address.uuid, name:Nordic.BLEAddressService.Address.name)
+                {(characteristicProfile) in
+                    characteristicProfile.initialValue = NSData.serialize(Nordic.BLEAddressService.Address.Value.fromRawValues([10, 11, 12, 13, 14, 15])?.toRawValues())
+                    characteristicProfile.properties = CBCharacteristicProperties.Read
+                })
+            // BLE Address Type
+            serviceProfile.addCharacteristic(EnumCharacteristicProfile<Nordic.BLEAddressService.AddressType.Value>(uuid:Nordic.BLEAddressService.AddressType.uuid, name:Nordic.BLEAddressService.AddressType.name)
+                {(characteristicProfile) in
+                    characteristicProfile.initialValue = NSData.serialize(Nordic.BLEAddressService.AddressType.Value.Public.toRaw())
+                    characteristicProfile.properties = CBCharacteristicProperties.Read
+                })
         })
 
     }
