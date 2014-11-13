321f729e335593b8d0ab3959c7bbdfb70090bdfc
Troy Stribling <troy.stribling@gmail.com>
2014-11-12 19:43:52 -0800

    UI tweaks

---
+++ BlueCapKit/Central/RegionConnectorator.swift
@@ -0,0 +1,15 @@
+//
+//  RegionConnectorator.swift
+//  BlueCapKit
+//
+//  Created by Troy Stribling on 11/12/14.
+//  Copyright (c) 2014 gnos.us. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import CoreBluetooth
+
+public class RegionConnectorator : Connectorator {
+    
+}
