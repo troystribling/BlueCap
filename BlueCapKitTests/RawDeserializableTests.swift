025aea2f20af0cd0d0e6879974a467954c56de47
Troy Stribling <troy.stribling@gmail.com>
2015-02-09 19:53:52 -0800

    added RawDeserializable tests

--- BlueCapKitTests/RawDeserializableTests.swift
+++ BlueCapKitTests/RawDeserializableTests.swift
@@ -6,16 +6,17 @@
 //  Copyright (c) 2015 gnos.us. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 import XCTest
 import BlueCapKit
 
 class RawDeserializableTests: XCTestCase {
 
-    public enum Enabled: UInt8, RawDeserializable {
+    public enum Testit: UInt8, RawDeserializable {
         case No     = 0
         case Yes    = 1
-        case Maybe  = 2
+        case Maybe  = 2        
+        static let uuid = "abc"
     }
 
     override func setUp() {
@@ -27,15 +28,25 @@ class RawDeserializableTests: XCTestCase {
     }
 
     func testSuccessfulDeserilaization() {
-        
+        let data = "02".dataFromHexString()
+        if let value : Testit = Serde.deserialize(data) {
+            XCTAssert(value == .Maybe, "RawDeserializable deserailization failed: \(data)")
+        } else {
+            XCTFail("RawDeserializable deserailization failed")
+        }
     }
 
     func testFailedDeserilaization() {
-        
+        let data = "03".dataFromHexString()
+        if let value : Testit = Serde.deserialize(data) {
+            XCTFail("RawDeserializable deserailization succeeded")
+        }
     }
     
     func testSerialization() {
-        
+        let value = Testit.Yes
+        let data = Serde.serialize(value)
+        XCTAssert(data.hexStringValue() == "01", "RawDeserializable serualization failed: \(data)")
     }
 
 }
