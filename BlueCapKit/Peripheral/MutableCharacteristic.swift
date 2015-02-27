91a200064eb6785c36a084a4eeb50de06e83dc2f
Troy Stribling <troy.stribling@gmail.com>
2015-02-27 08:52:39 -0800

    more work on mutablke characteristic refactor

--- BlueCapKit/Peripheral/MutableCharacteristic.swift
+++ BlueCapKit/Peripheral/MutableCharacteristic.swift
@@ -12,35 +12,47 @@ import CoreBluetooth
 ///////////////////////////////////////////
 // MutableCharacteristicImpl
 public protocol MutableCharacteristicWrappable {
+    
+    typealias RequestWrapper
+    typealias ResultWrapper
   
-    var uuid            : CBUUID!   {get}
-    var name            : String    {get}
-    var value           : NSData!   {get}
-    var stringValues    : [String]  {get}
+    var uuid            : CBUUID            {get}
+    var name            : String            {get}
+    var value           : NSData!           {get}
+    var stringValues    : [String]          {get}
+    var stringValue     : [String:String]?  {get}
 
     func propertyEnabled(property:CBCharacteristicProperties) -> Bool
     func permissionEnabled(permission:CBAttributePermissions) -> Bool
     
-    func stringValue(data:NSData?) -> [String:String]?
     func dataFromStringValue(stringValue:[String:String]) -> NSData?
     
     func updateValueWithData(value:NSData)
+    func respondToRequest(request:RequestWrapper, withResult result:ResultWrapper)
 }
 
-public final class MutableCharacteristicImpl<Wrapper:MutableCharacteristicWrappable> {
+public protocol CBATTRequestWrappable {
     
-    internal var processWriteRequestPromise : StreamPromise<CBATTRequest>?
+}
+
+public protocol CBATTErrorWrappable {
     
+}
+
+public final class MutableCharacteristicImpl<Wrapper where Wrapper:MutableCharacteristicWrappable,
+                                                           Wrapper.RequestWrapper:CBATTRequestWrappable,
+                                                           Wrapper.ResultWrapper:CBATTErrorWrappable> {
+    
+    internal var processWriteRequestPromise : StreamPromise<Wrapper.RequestWrapper>?
     
     public init() {
     }
     
-    
-    public func startProcessingWriteRequests(capacity:Int? = nil) -> FutureStream<CBATTRequest> {
+    public func startProcessingWriteRequests(capacity:Int? = nil) -> FutureStream<Wrapper.RequestWrapper> {
         if let capacity = capacity {
-            self.processWriteRequestPromise = StreamPromise<CBATTRequest>(capacity:capacity)
+            self.processWriteRequestPromise = StreamPromise<Wrapper.RequestWrapper>(capacity:capacity)
         } else {
-            self.processWriteRequestPromise = StreamPromise<CBATTRequest>()
+            self.processWriteRequestPromise = StreamPromise<Wrapper.RequestWrapper>()
         }
         return self.processWriteRequestPromise!.future
     }
@@ -49,8 +61,8 @@ public final class MutableCharacteristicImpl<Wrapper:MutableCharacteristicWrappa
         self.processWriteRequestPromise = nil
     }
     
-    public func respondToRequest(request:CBATTRequest, withResult result:CBATTError) {
-        PeripheralManager.sharedInstance.cbPeripheralManager.respondToRequest(request, withResult:result)
+    public func respondToRequest(characteristic:Wrapper, request:Wrapper.RequestWrapper, withResult result:Wrapper.ResultWrapper) {
+        characteristic.respondToRequest(request, withResult:result)
     }
     
     
@@ -73,28 +85,27 @@ public final class MutableCharacteristicImpl<Wrapper:MutableCharacteristicWrappa
     public func updateValue<T:RawArrayPairDeserializable>(characteristic:Wrapper, value:T) {
         return characteristic.updateValueWithData(Serde.serialize(value))
     }
+    
+    public func processWriteRequest(request:Wrapper.RequestWrapper) {
+        if let processWriteRequestPromise = self.processWriteRequestPromise {
+            processWriteRequestPromise.success(request)
+        }
+    }
 
 }
 // MutableCharacteristicImpl
 ///////////////////////////////////////////
+extension CBATTRequest : CBATTRequestWrappable {
+}
 
+extension CBATTError : CBATTErrorWrappable {
+}
 
-public class MutableCharacteristic : NSObject {
+public class MutableCharacteristic : MutableCharacteristicWrappable {
+
+    var impl = MutableCharacteristicImpl<MutableCharacteristic>()
     
-    private let profile                         : CharacteristicProfile!
-    private var _value                          : NSData?
-    
-    internal let cbMutableChracteristic         : CBMutableCharacteristic!
-    internal var processWriteRequestPromise     : StreamPromise<CBATTRequest>?
-    
-    public var permissions : CBAttributePermissions {
-        return self.cbMutableChracteristic.permissions
-    }
-    
-    public var properties : CBCharacteristicProperties {
-        return self.cbMutableChracteristic.properties
-    }
-    
+    // MutableCharacteristicImpl
     public var uuid : CBUUID {
         return self.profile.uuid
     }
@@ -111,13 +122,54 @@ public class MutableCharacteristic : NSObject {
             self._value = newValue
         }
     }
+
+    public var stringValues : [String] {
+        return self.profile.stringValues
+    }
     
+    public func propertyEnabled(property:CBCharacteristicProperties) -> Bool {
+        return (self.properties.rawValue & property.rawValue) > 0
+    }
+    
+    public func permissionEnabled(permission:CBAttributePermissions) -> Bool {
+        return (self.permissions.rawValue & permission.rawValue) > 0
+    }
+
     public var stringValue : [String:String]? {
         if self.value != nil {
             return self.profile.stringValue(self.value)
         } else {
             return nil
         }
+    }
+    
+    public func dataFromStringValue(stringValue:[String:String]) -> NSData? {
+        return self.profile.dataFromStringValue(stringValue)
+    }
+    
+    public func updateValueWithData(value:NSData) {
+        self._value = value
+        PeripheralManager.sharedInstance.cbPeripheralManager.updateValue(value, forCharacteristic:self.cbMutableChracteristic, onSubscribedCentrals:nil)
+    }
+    
+    public func respondToRequest(request:CBATTRequest, withResult result:CBATTError) {
+        self.impl.respondToRequest(self, request:request, withResult:result)
+    }
+    
+    
+    // MutableCharacteristicImpl
+
+    private let profile                         : CharacteristicProfile!
+    private var _value                          : NSData?
+    
+    internal let cbMutableChracteristic         : CBMutableCharacteristic!
+    
+    public var permissions : CBAttributePermissions {
+        return self.cbMutableChracteristic.permissions
+    }
+    
+    public var properties : CBCharacteristicProperties {
+        return self.cbMutableChracteristic.properties
     }
     
     public class func withProfiles(profiles:[CharacteristicProfile]) -> [MutableCharacteristic] {
@@ -128,45 +180,22 @@ public class MutableCharacteristic : NSObject {
         self.profile = profile
         self._value = profile.initialValue
         self.cbMutableChracteristic = CBMutableCharacteristic(type:profile.uuid, properties:profile.properties, value:nil, permissions:profile.permissions)
-        super.init()
     }
 
     public convenience init(uuid:String) {
         self.init(profile:CharacteristicProfile(uuid:uuid))
     }
     
-    public var stringValues : [String] {
-        return self.profile.stringValues
-    }
-    
     public func startProcessingWriteRequests(capacity:Int? = nil) -> FutureStream<CBATTRequest> {
-        if let capacity = capacity {
-            self.processWriteRequestPromise = StreamPromise<CBATTRequest>(capacity:capacity)
-        } else {
-            self.processWriteRequestPromise = StreamPromise<CBATTRequest>()
-        }
-        return self.processWriteRequestPromise!.future
+        return self.impl.startProcessingWriteRequests(capacity:capacity)
     }
     
     public func stopProcessingWriteRequests() {
-        self.processWriteRequestPromise = nil
+        self.impl.stopProcessingWriteRequests()
     }
     
-    public func respondToRequest(request:CBATTRequest, withResult result:CBATTError) {
-        PeripheralManager.sharedInstance.cbPeripheralManager.respondToRequest(request, withResult:result)
-    }
-    
-    public func propertyEnabled(property:CBCharacteristicProperties) -> Bool {
-        return (self.properties.rawValue & property.rawValue) > 0
-    }
-    
-    public func permissionEnabled(permission:CBAttributePermissions) -> Bool {
-        return (self.permissions.rawValue & permission.rawValue) > 0
-    }
-    
-    public func updateValueWithData(value:NSData) {
-        self._value = value
-        PeripheralManager.sharedInstance.cbPeripheralManager.updateValue(value, forCharacteristic:self.cbMutableChracteristic, onSubscribedCentrals:nil)
+    public func processWriteRequest(request:CBATTRequest)  {
+        self.impl.processWriteRequest(request)
     }
     
     public func updateValueWithString(value:Dictionary<String, String>) {
@@ -178,23 +207,23 @@ public class MutableCharacteristic : NSObject {
     }
     
     public func updateValue<T:Deserializable>(value:T) {
-        return self.updateValueWithData(Serde.serialize(value))
+        return self.impl.updateValue(self, value:value)
     }
 
     public func updateValue<T:RawDeserializable>(value:T) {
-        return self.updateValueWithData(Serde.serialize(value))
+        return self.impl.updateValue(self, value:value)
     }
 
     public func updateValue<T:RawArrayDeserializable>(value:T) {
-        return self.updateValueWithData(Serde.serialize(value))
+        return self.impl.updateValue(self, value:value)
     }
 
     public func updateValue<T:RawPairDeserializable>(value:T) {
-        return self.updateValueWithData(Serde.serialize(value))
+        return self.impl.updateValue(self, value:value)
     }
 
     public func updateValue<T:RawArrayPairDeserializable>(value:T) {
-        return self.updateValueWithData(Serde.serialize(value))
+        return self.impl.updateValue(self, value:value)
     }
 
 }
