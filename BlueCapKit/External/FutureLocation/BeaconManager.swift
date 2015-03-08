1390a230e8cb73093807bc18f1bfde35d3d36ea6
Troy Stribling <troy.stribling@gmail.com>
2015-03-08 11:52:25 -0700

    updated future location

--- BlueCapKit/External/FutureLocation/BeaconManager.swift
+++ BlueCapKit/External/FutureLocation/BeaconManager.swift
@@ -9,32 +9,54 @@
 import Foundation
 import CoreLocation
 
-public class BeaconManager : RegionManager {
-    
-    private var regionRangingStatus         : [String:Bool]                     = [:]
-    internal var configuredBeaconRegions    : [CLBeaconRegion:BeaconRegion]     = [:]
+/////////////////////////////////////////////
+// BeaconManagerImpl
+public protocol BeaconManagerWrappable {
 
-    public var beaconRegions : [BeaconRegion] {
-        return self.configuredBeaconRegions.values.array
-    }
+    typealias WrappedBeaconRegion
+
+    var beaconRegions : [WrappedBeaconRegion] {get}
     
+    func wrappedStartRangingBeaconsInRegion(beaconRegion:WrappedBeaconRegion)
+    func wrappedStopRangingBeaconsInRegion(beaconRegion:WrappedBeaconRegion)
+    
+}
+
+public protocol BeaconRegionWrappable {
+    
+    typealias WrappedBeacon
+    
+    var identifier     : String                         {get}
+    var beaconPromise  : StreamPromise<[WrappedBeacon]> {get}
+    
+}
+
+public protocol BeaconWrappable {
+}
+
+extension BeaconRegion : BeaconRegionWrappable {
+}
+
+extension Beacon : BeaconWrappable {
+}
+
+public class BeaconManagerImpl<Wrapper where
+                                Wrapper:BeaconManagerWrappable,
+                                Wrapper.WrappedBeaconRegion:BeaconRegionWrappable,
+                                Wrapper.WrappedBeaconRegion.WrappedBeacon : BeaconWrappable,
+                                Wrapper:RegionManagerWrappable,
+                                Wrapper:LocationManagerWrappable,
+                                Wrapper.WrappedCLLocation:CLLocationWrappable,
+                                Wrapper.WrappedRegion:RegionWrappable> : RegionManagerImpl<Wrapper> {
+
+    private var regionRangingStatus = [String:Bool]()
+
     public var isRanging : Bool {
         return self.regionRangingStatus.values.array.any{$0}
     }
     
     public override init() {
         super.init()
-    }
-
-    public class func isRangingAvailable() -> Bool {
-        return CLLocationManager.isRangingAvailable()
-    }
-    
-    public override class var sharedInstance : BeaconManager {
-        struct Static {
-            static let instance = BeaconManager()
-        }
-        return Static.instance
     }
     
     public func isRangingRegion(identifier:String) -> Bool {
@@ -44,7 +66,106 @@ public class BeaconManager : RegionManager {
             return false
         }
     }
+    
+    // control
+    public func startRangingBeaconsInRegion(manager:Wrapper, authorization:CLAuthorizationStatus, beaconRegion:Wrapper.WrappedBeaconRegion) -> FutureStream<[Wrapper.WrappedBeaconRegion.WrappedBeacon]> {
+        let authoriztaionFuture = self.authorize(manager, authorization:authorization)
+        authoriztaionFuture.onSuccess {status in
+            self.regionRangingStatus[beaconRegion.identifier] = true
+            manager.wrappedStartRangingBeaconsInRegion(beaconRegion)
+        }
+        authoriztaionFuture.onFailure {error in
+            beaconRegion.beaconPromise.failure(error)
+        }
+        return beaconRegion.beaconPromise.future
+    }
+    
+    public func startRangingBeaconsInRegion(manager:Wrapper, beaconRegion:Wrapper.WrappedBeaconRegion) -> FutureStream<[Wrapper.WrappedBeaconRegion.WrappedBeacon]> {
+        return self.startRangingBeaconsInRegion(manager, authorization:CLAuthorizationStatus.AuthorizedAlways, beaconRegion:beaconRegion)
+    }
+    
+    public func stopRangingBeaconsInRegion(manager:Wrapper, beaconRegion:Wrapper.WrappedBeaconRegion) {
+        self.regionRangingStatus.removeValueForKey(beaconRegion.identifier)
+        manager.wrappedStartRangingBeaconsInRegion(beaconRegion)
+    }
+    
+    public func resumeRangingAllBeacons(manager:Wrapper) {
+        for beaconRegion in manager.beaconRegions {
+            self.regionRangingStatus[beaconRegion.identifier] = true
+            manager.wrappedStartRangingBeaconsInRegion(beaconRegion)
+        }
+    }
+    
+    public func pauseRangingAllBeacons(manager:Wrapper) {
+        for beaconRegion in manager.beaconRegions {
+            self.regionRangingStatus[beaconRegion.identifier] = false
+            manager.wrappedStopRangingBeaconsInRegion(beaconRegion)
+        }
+    }
+    
+    public func stopRangingAllBeacons(manager:Wrapper) {
+        for beaconRegion in manager.beaconRegions {
+            manager.wrappedStopRangingBeaconsInRegion(beaconRegion)
+        }
+    }
+    
+    // CLLocationManagerDelegate
+    public func didRangeBeacons(beacons:[Wrapper.WrappedBeaconRegion.WrappedBeacon], inRegion region:Wrapper.WrappedBeaconRegion) {
+        Logger.debug("BeaconManager#didRangeBeacons: \(region.identifier)")
+        region.beaconPromise.success(beacons)
+    }
+    
+    public func rangingBeaconsDidFailForRegion(region:Wrapper.WrappedBeaconRegion, withError error:NSError!) {
+        Logger.debug("BeaconManager#rangingBeaconsDidFailForRegion: \(region.identifier)")
+        region.beaconPromise.failure(error)
+    }
 
+}
+// BeaconManagerImpl
+/////////////////////////////////////////////
+
+
+public class BeaconManager : RegionManager, BeaconManagerWrappable {
+    
+    let beaconImpl = BeaconManagerImpl<BeaconManager>()
+    
+    // BeaconManagerWrappable
+    public var beaconRegions : [BeaconRegion] {
+        return self.configuredBeaconRegions.values.array
+    }
+    
+    public func wrappedStartRangingBeaconsInRegion(beaconRegion:BeaconRegion) {
+        self.configuredBeaconRegions[beaconRegion.clBeaconRegion] = beaconRegion
+        self.clLocationManager.startRangingBeaconsInRegion(beaconRegion.clBeaconRegion)
+    }
+    
+    public func wrappedStopRangingBeaconsInRegion(beaconRegion:BeaconRegion) {
+        self.configuredBeaconRegions.removeValueForKey(beaconRegion.clBeaconRegion)
+        self.clLocationManager.stopRangingBeaconsInRegion(beaconRegion.clBeaconRegion)
+    }
+
+    // BeaconManagerWrappable
+    internal var configuredBeaconRegions    : [CLBeaconRegion:BeaconRegion]     = [:]
+
+    public class func isRangingAvailable() -> Bool {
+        return CLLocationManager.isRangingAvailable()
+    }
+    
+    public var isRanging : Bool {
+        return self.beaconImpl.isRanging
+    }
+    
+    public override init() {
+        super.init()
+    }
+
+    public override class var sharedInstance : BeaconManager {
+        struct Static {
+            static let instance = BeaconManager()
+        }
+        return Static.instance
+    }
+    
     public func beaconRegion(identifier:String) -> BeaconRegion? {
         let regions = self.configuredBeaconRegions.keys.array.filter{$0.identifier == identifier}
         if let region = regions.first {
@@ -53,49 +174,34 @@ public class BeaconManager : RegionManager {
             return nil
         }
     }
-    
+
+    public func isRangingRegion(identifier:String) -> Bool {
+        return self.beaconImpl.isRangingRegion(identifier)
+    }
+
     // control
     public func startRangingBeaconsInRegion(authorization:CLAuthorizationStatus, beaconRegion:BeaconRegion) -> FutureStream<[Beacon]> {
-        let authoriztaionFuture = self.authorize(authorization)
-        authoriztaionFuture.onSuccess {status in
-            self.regionRangingStatus[beaconRegion.identifier] = true
-            self.configuredBeaconRegions[beaconRegion.clBeaconRegion] = beaconRegion
-            self.clLocationManager.startRangingBeaconsInRegion(beaconRegion.clBeaconRegion)
-        }
-        authoriztaionFuture.onFailure {error in
-            beaconRegion.beaconPromise.failure(error)
-        }
-        return beaconRegion.beaconPromise.future
+        return self.beaconImpl.startRangingBeaconsInRegion(self, authorization:authorization, beaconRegion:beaconRegion)
     }
 
     public func startRangingBeaconsInRegion(beaconRegion:BeaconRegion) -> FutureStream<[Beacon]> {
-       return self.startRangingBeaconsInRegion(CLAuthorizationStatus.AuthorizedAlways, beaconRegion:beaconRegion)
+        return self.beaconImpl.startRangingBeaconsInRegion(self, beaconRegion:beaconRegion)
     }
 
     public func stopRangingBeaconsInRegion(beaconRegion:BeaconRegion) {
-        self.regionRangingStatus.removeValueForKey(beaconRegion.identifier)
-        self.configuredBeaconRegions.removeValueForKey(beaconRegion.clBeaconRegion)
-        self.clLocationManager.stopRangingBeaconsInRegion(beaconRegion.clBeaconRegion)
+        self.beaconImpl.stopRangingBeaconsInRegion(self, beaconRegion:beaconRegion)
     }
     
     public func resumeRangingAllBeacons() {
-        for beaconRegion in self.beaconRegions {
-            self.regionRangingStatus[beaconRegion.identifier] = true
-            self.clLocationManager.startRangingBeaconsInRegion(beaconRegion.clBeaconRegion)
-        }
+        self.beaconImpl.resumeRangingAllBeacons(self)
     }
     
     public func pauseRangingAllBeacons() {
-        for beaconRegion in self.beaconRegions {
-            self.regionRangingStatus[beaconRegion.identifier] = false
-            self.clLocationManager.stopRangingBeaconsInRegion(beaconRegion.clBeaconRegion)
-        }
+        self.beaconImpl.pauseRangingAllBeacons(self)
     }
 
     public func stopRangingAllBeacons() {
-        for beaconRegion in self.beaconRegions {
-            self.stopRangingBeaconsInRegion(beaconRegion)
-        }
+        self.beaconImpl.stopRangingAllBeacons(self)
     }
 
     public func requestStateForRegion(beaconMonitor:BeaconRegion) {
@@ -108,17 +214,15 @@ public class BeaconManager : RegionManager {
         if let beaconRegion = self.configuredBeaconRegions[region] {
             let bcbeacons = beacons.map{Beacon(clbeacon:($0 as! CLBeacon))}
             beaconRegion._beacons = bcbeacons
-            beaconRegion.beaconPromise.success(bcbeacons)
+            self.beaconImpl.didRangeBeacons(bcbeacons, inRegion:beaconRegion)
         }
     }
     
     public func locationManager(_:CLLocationManager!, rangingBeaconsDidFailForRegion region:CLBeaconRegion!, withError error:NSError!) {
         Logger.debug("BeaconManager#rangingBeaconsDidFailForRegion: \(region.identifier)")
         if let beaconRegion = self.configuredBeaconRegions[region] {
-            beaconRegion.beaconPromise.failure(error)
+            self.beaconImpl.rangingBeaconsDidFailForRegion(beaconRegion, withError:error)
         }
     }
     
 }
-
-private var thisBeaconManager : BeaconManager?
--- BlueCapKit/External/FutureLocation/RegionManager.swift
+++ BlueCapKit/External/FutureLocation/RegionManager.swift
@@ -9,23 +9,160 @@
 import Foundation
 import CoreLocation
 
-public class RegionManager : LocationManager {
+/////////////////////////////////////////////
+// RegionManagerImpl
+public protocol RegionManagerWrappable {
+    
+    typealias WrappedRegion
+    
+    var regions : [WrappedRegion] {get}
+    
+    func region(identifier:String) -> WrappedRegion?
+    func wrappedStartMonitoringForRegion(region:WrappedRegion)
+    func wrappedStopMonitoringForRegion(region:WrappedRegion)
+    
+}
 
-    internal var configuredRegions       : [CLRegion:Region] = [:]
-    internal var regionMonitorStatus     : [String:Bool]     = [:]
+public protocol RegionWrappable {
+    var regionPromise   : StreamPromise<RegionState> {get}
+    var identifier      : String {get}
+}
+
+extension Region : RegionWrappable {
+}
+
+public class RegionManagerImpl<Wrapper where Wrapper:RegionManagerWrappable,
+                               Wrapper:LocationManagerWrappable,
+                               Wrapper.WrappedCLLocation:CLLocationWrappable,
+                               Wrapper.WrappedRegion:RegionWrappable> : LocationManagerImpl<Wrapper> {
     
-    public var regions : [Region] {
-        return self.configuredRegions.values.array
-    }
-    
-    public var maximumRegionMonitoringDistance : CLLocationDistance {
-        return self.clLocationManager.maximumRegionMonitoringDistance
-    }
+    internal var regionMonitorStatus = [String:Bool]()
     
     public var isMonitoring : Bool {
         return self.regionMonitorStatus.values.array.any{$0}
     }
     
+    public override init() {
+    }
+    
+    public func isMonitoringRegion(identifier:String) -> Bool {
+        if let status = self.regionMonitorStatus[identifier] {
+            return status
+        } else {
+            return false
+        }
+    }
+    
+    // control
+    public func startMonitoringForRegion(manager:Wrapper, authorization:CLAuthorizationStatus, region:Wrapper.WrappedRegion) -> FutureStream<RegionState> {
+        let authoriztaionFuture = self.authorize(manager, authorization:authorization)
+        authoriztaionFuture.onSuccess {status in
+            self.regionMonitorStatus[region.identifier] = true
+            manager.wrappedStartMonitoringForRegion(region)
+        }
+        authoriztaionFuture.onFailure {error in
+            region.regionPromise.failure(error)
+        }
+        return region.regionPromise.future
+    }
+    
+    public func startMonitoringForRegion(manager:Wrapper, region:Wrapper.WrappedRegion) -> FutureStream<RegionState> {
+        return self.startMonitoringForRegion(manager, authorization:CLAuthorizationStatus.AuthorizedAlways, region:region)
+    }
+    
+    public func stopMonitoringForRegion(manager:Wrapper, region:Wrapper.WrappedRegion) {
+        self.regionMonitorStatus.removeValueForKey(region.identifier)
+        manager.wrappedStopMonitoringForRegion(region)
+    }
+    
+    public func resumeMonitoringAllRegions(manager:Wrapper) {
+        for region in manager.regions {
+            self.regionMonitorStatus[region.identifier] = true
+            manager.wrappedStartMonitoringForRegion(region)
+        }
+    }
+    
+    public func pauseMonitoringAllRegions(manager:Wrapper) {
+        for region in manager.regions {
+            self.regionMonitorStatus[region.identifier] = false
+            manager.wrappedStopMonitoringForRegion(region)
+        }
+    }
+    
+    public func stopMonitoringAllRegions(manager:Wrapper) {
+        for region in manager.regions {
+            manager.wrappedStopMonitoringForRegion(region)
+        }
+    }
+    
+    // CLLocationManagerDelegate
+    public func didEnterRegion(region:Wrapper.WrappedRegion) {
+        Logger.debug("RegionManagerImpl#didEnterRegion: \(region.identifier)")
+        region.regionPromise.success(.Inside)
+    }
+    
+    public func didExitRegion(region:Wrapper.WrappedRegion) {
+        Logger.debug("RegionManagerImpl#didExitRegion: \(region.identifier)")
+        region.regionPromise.success(.Outside)
+    }
+    
+    public func didDetermineState(state:CLRegionState, forRegion region:Wrapper.WrappedRegion) {
+        Logger.debug("RegionManagerImpl#didDetermineState: \(region.identifier)")
+    }
+    
+    public func monitoringDidFailForRegion(region:Wrapper.WrappedRegion, withError error:NSError!) {
+        Logger.debug("RegionManagerImpl#monitoringDidFailForRegion: \(region.identifier)")
+        region.regionPromise.failure(error)
+    }
+    
+    public func didStartMonitoringForRegion(region:Wrapper.WrappedRegion) {
+        Logger.debug("RegionManagerImpl#didStartMonitoringForRegion: \(region.identifier)")
+        region.regionPromise.success(.Start)
+    }
+}
+
+// RegionManagerImpl
+/////////////////////////////////////////////
+
+public class RegionManager : LocationManager, RegionManagerWrappable {
+
+    let regionImpl = RegionManagerImpl<RegionManager>()
+
+    // RegionManagerWrappable
+    public var regions : [Region] {
+        return self.configuredRegions.values.array
+    }
+
+    public func region(identifier:String) -> Region? {
+        let regions = self.configuredRegions.keys.array.filter{$0.identifier == identifier}
+        if let region = regions.first {
+            return self.configuredRegions[region]
+        } else {
+            return nil
+        }
+    }
+    
+    public func wrappedStartMonitoringForRegion(region:Region) {
+        self.configuredRegions[region.clRegion] = region
+        self.clLocationManager.startMonitoringForRegion(region.clRegion)
+    }
+    
+    public func wrappedStopMonitoringForRegion(region:Region) {
+        self.configuredRegions.removeValueForKey(region.clRegion)
+        self.clLocationManager.stopMonitoringForRegion(region.clRegion)
+    }
+    // RegionManagerWrappable
+    
+    internal var configuredRegions : [CLRegion:Region] = [:]
+    
+    public var maximumRegionMonitoringDistance : CLLocationDistance {
+        return self.clLocationManager.maximumRegionMonitoringDistance
+    }
+
+    public var isMonitoring : Bool {
+        return self.regionImpl.isMonitoring
+    }
+
     public class var sharedInstance : RegionManager {
         struct Static {
             static let instance = RegionManager()
@@ -38,98 +175,67 @@ public class RegionManager : LocationManager {
     }
     
     public func isMonitoringRegion(identifier:String) -> Bool {
-        if let status = self.regionMonitorStatus[identifier] {
-            return status
-        } else {
-            return false
-        }
-    }
-    
-    public func region(identifier:String) -> Region? {
-        let regions = self.configuredRegions.keys.array.filter{$0.identifier == identifier}
-        if let region = regions.first {
-            return self.configuredRegions[region]
-        } else {
-            return nil
-        }
+        return self.regionImpl.isMonitoringRegion(identifier)
     }
 
     // control
     public func startMonitoringForRegion(authorization:CLAuthorizationStatus, region:Region) -> FutureStream<RegionState> {
-        let authoriztaionFuture = self.authorize(authorization)
-        authoriztaionFuture.onSuccess {status in
-            self.regionMonitorStatus[region.identifier] = true
-            self.configuredRegions[region.clRegion] = region
-            self.clLocationManager.startMonitoringForRegion(region.clRegion)
-        }
-        authoriztaionFuture.onFailure {error in
-            region.regionPromise.failure(error)
-        }
-        return region.regionPromise.future
+        return self.regionImpl.startMonitoringForRegion(self, authorization:authorization, region:region)
     }
 
     public func startMonitoringForRegion(region:Region) -> FutureStream<RegionState> {
-        return self.startMonitoringForRegion(CLAuthorizationStatus.AuthorizedAlways, region:region)
+        return self.regionImpl.startMonitoringForRegion(self, authorization:CLAuthorizationStatus.AuthorizedAlways, region:region)
     }
 
     public func stopMonitoringForRegion(region:Region) {
-        self.regionMonitorStatus.removeValueForKey(region.identifier)
-        self.configuredRegions.removeValueForKey(region.clRegion)
-        self.clLocationManager.stopMonitoringForRegion(region.clRegion)
+        self.regionImpl.stopMonitoringForRegion(self, region:region)
     }
     
     public func resumeMonitoringAllRegions() {
-        for region in self.regions {
-            self.regionMonitorStatus[region.identifier] = true
-            self.clLocationManager.startMonitoringForRegion(region.clRegion)
-        }
+        self.regionImpl.resumeMonitoringAllRegions(self)
     }
     
     public func pauseMonitoringAllRegions() {
-        for region in self.regions {
-            self.regionMonitorStatus[region.identifier] = false
-            self.clLocationManager.stopMonitoringForRegion(region.clRegion)
-        }
+        self.regionImpl.pauseMonitoringAllRegions(self)
     }
 
     public func stopMonitoringAllRegions() {
-        for region in self.regions {
-            self.stopMonitoringForRegion(region)
-        }
+        self.regionImpl.stopMonitoringAllRegions(self)
     }
 
     // CLLocationManagerDelegate
     public func locationManager(_:CLLocationManager!, didEnterRegion region:CLRegion!) {
         Logger.debug("RegionManager#didEnterRegion: \(region.identifier)")
         if let bcregion = self.configuredRegions[region] {
-            bcregion.regionPromise.success(.Inside)
+           self.regionImpl.didEnterRegion(bcregion)
         }
     }
     
     public func locationManager(_:CLLocationManager!, didExitRegion region:CLRegion!) {
         Logger.debug("RegionManager#didExitRegion: \(region.identifier)")
         if let bcregion = self.configuredRegions[region] {
-            bcregion.regionPromise.success(.Outside)
+            self.regionImpl.didExitRegion(bcregion)
         }
     }
     
     public func locationManager(_:CLLocationManager!, didDetermineState state:CLRegionState, forRegion region:CLRegion!) {
         Logger.debug("RegionManager#didDetermineState: \(region.identifier)")
         if let bcregion = self.configuredRegions[region] {
+            self.regionImpl.didDetermineState(state, forRegion:bcregion)
         }
     }
     
     public func locationManager(_:CLLocationManager!, monitoringDidFailForRegion region:CLRegion!, withError error:NSError!) {
         Logger.debug("RegionManager#monitoringDidFailForRegion: \(region.identifier)")
         if let bcregion = self.configuredRegions[region] {
-            bcregion.regionPromise.failure(error)
+            self.regionImpl.monitoringDidFailForRegion(bcregion, withError:error)
         }
     }
     
     public func locationManager(_:CLLocationManager!, didStartMonitoringForRegion region:CLRegion!) {
         Logger.debug("RegionManager#didStartMonitoringForRegion: \(region.identifier)")
         if let bcregion = self.configuredRegions[region] {
-            bcregion.regionPromise.success(.Start)
+            self.regionImpl.didStartMonitoringForRegion(bcregion)
         }
     }
 }
