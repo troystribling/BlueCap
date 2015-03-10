157aeb4ed63a3b779272127f505610428a545159
Troy Stribling <troy.stribling@gmail.com>
2015-03-10 08:35:35 -0700

    fixed failing tests after serialize changes

--- BlueCapKitTests/SerializableTests.swift
+++ BlueCapKitTests/SerializableTests.swift
@@ -23,57 +23,57 @@ class SerializableTests: XCTestCase {
 
     func testSerializeUInt8() {
         let data = Serde.serialize(UInt8(100))
-        XCTAssert(data.hexStringValue() == "64", "UInt8 serialization value invalid: \(data)")
+        XCTAssert(data.hexStringValue() == "64", "UInt8 serialization value invalid: \(data.hexStringValue())")
     }
     
     func testSerializeInt8() {
         let data = Serde.serialize(Int8(-100))
-        XCTAssert(data.hexStringValue() == "9c", "Int8 serialization value invalid: \(data)")
+        XCTAssert(data.hexStringValue() == "9c", "Int8 serialization value invalid: \(data.hexStringValue())")
     }
     
     func testSerializeUInt16() {
         let data = Serde.serialize(UInt16(1000))
-        XCTAssert(data.hexStringValue() == "e803", "UInt16 serialization value invalid: \(data)")
+        XCTAssert(data.hexStringValue() == "e803", "UInt16 serialization value invalid: \(data.hexStringValue())")
     }
 
     func testSerializeInt16() {
         let data = Serde.serialize(Int16(-1100))
-        XCTAssert(data.hexStringValue() == "b4fb", "Int16 serialization value invalid: \(data)")
+        XCTAssert(data.hexStringValue() == "b4fb", "Int16 serialization value invalid: \(data.hexStringValue())")
     }
     
     func testSerializeUInt8Array() {
         let value : [UInt8] = [100, 10]
         let data = Serde.serialize(value)
-        XCTAssert(data.hexStringValue() == "640a", "UInt8 array serialization value invalid: \(data)")
+        XCTAssert(data.hexStringValue() == "640a", "UInt8 array serialization value invalid: \(data.hexStringValue())")
     }
 
     func testSerializeInt8Array() {
         let value : [Int8] = [-100, 10]
         let data = Serde.serialize(value)
-        XCTAssert(data.hexStringValue() == "9c0a", "Int8 array serialization value invalid: \(data)")
+        XCTAssert(data.hexStringValue() == "9c0a", "Int8 array serialization value invalid: \(data.hexStringValue())")
     }
     
     func testSerializeUInt16Array() {
         let value : [UInt16] = [1000, 100]
         let data = Serde.serialize(value)
-        XCTAssert(data.hexStringValue() == "e8036400", "UInt16 array serialization value invalid: \(data)")
+        XCTAssert(data.hexStringValue() == "e8036400", "UInt16 array serialization value invalid: \(data.hexStringValue())")
     }
 
     func testSerializeInt16Array() {
         let value : [Int16] = [-1100, 100]
         let data = Serde.serialize(value)
-        XCTAssert(data.hexStringValue() == "b4fb6400", "Int16 array serialization value invalid: \(data)")
+        XCTAssert(data.hexStringValue() == "b4fb6400", "Int16 array serialization value invalid: \(data.hexStringValue())")
     }
 
     func testSerializePair() {
-        let value = (Int16(-1100), UInt8(100))
-        let data = NSData.serialize(value)
-        XCTAssert(data.hexStringValue() == "b4fb64", "Pair serialization value invalid: \(data)")
+        let data = NSData.serialize(Int16(-1100),  value2:UInt8(100))
+        XCTAssert(data.hexStringValue() == "b4fb64", "Pair serialization value invalid: \(data.hexStringValue())")
     }
 
     func testSerializeArrayPair() {
-        let value = ([Int16(-1100), Int16(1000)] , [UInt8(100), UInt8(75)])
-        let data = NSData.serialize(value)
-        XCTAssert(data.hexStringValue() == "b4fbe803644b", "Pair serialization value invalid: \(data)")
+        let value1 = [Int16(-1100), Int16(1000)]
+        let value2 = [UInt8(100), UInt8(75)]
+        let data = NSData.serializeArrays(value1, values2:value2)
+        XCTAssert(data.hexStringValue() == "b4fbe803644b", "Pair serialization value invalid: \(data.hexStringValue())")
     }
 }
