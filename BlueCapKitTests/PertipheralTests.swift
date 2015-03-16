f6c22375317c6c9a642c2c8d13d2515c7f0de6f8
Troy Stribling <troy.stribling@gmail.com>
2015-03-16 08:45:43 -0700

    added peripheral mocks

--- BlueCapKitTests/PertipheralTests.swift
+++ BlueCapKitTests/PertipheralTests.swift
@@ -8,9 +8,89 @@
 
 import UIKit
 import XCTest
+import CoreBluetooth
 import BlueCapKit
 
 class PertipheralTests: XCTestCase {
+
+    // PeripheralMock
+    struct MockValues {
+        static var state :CBPeripheralState = .Connected
+        static var connectorator : Connectorator? = nil
+    }
+    
+    struct PeripheralMock : PeripheralWrappable {
+        
+        var name : String {
+            return "Mock Periphearl"
+        }
+        
+        var state: CBPeripheralState {
+            return MockValues.state
+        }
+        
+        var connectorator : Connectorator? {
+            return MockValues.connectorator
+        }
+        
+        var services : [ServiceMock] {
+            return [ServiceMock(uuid:CBUUID(string:"2f0a0017-69aa-f316-3e78-4194989a6ccc"), name:"Service Mock-1"),
+                    ServiceMock(uuid:CBUUID(string:"2f0a0017-69aa-f316-3e78-4194989a6aaa"), name:"Service Mock-1")]
+        }
+        
+        init() {            
+        }
+        
+        func connect() {
+        }
+        
+        func cancel() {
+        }
+        
+        func disconnect() {
+        }
+        
+        func discoverServices(services:[CBUUID]!) {
+        }
+        
+        func didDiscoverServices() {
+        }
+
+    }
+
+    struct ServiceMock : ServiceWrappable {
+        
+        let uuid:CBUUID!
+        let name:String
+        
+        let impl = ServiceImpl<ServiceMock>()
+        
+        init(uuid:CBUUID, name:String) {
+            self.uuid = uuid
+            self.name = name
+        }
+        
+        var state: CBPeripheralState {
+            return MockValues.state
+        }
+        
+        func discoverCharacteristics(characteristics:[CBUUID]!) {
+        }
+        
+        func didDiscoverCharacteristics(error:NSError!) {
+        }
+        
+        func createCharacteristics() {
+        }
+        
+        func discoverAllCharacteristics() -> Future<ServiceMock> {
+            return self.impl.discoverIfConnected(self, characteristics:nil)
+        }
+        
+    }
+    // PeripheralMock
+    let mock = PeripheralMock()
+    let impl = PeripheralImpl<PeripheralMock>()
 
     override func setUp() {
         super.setUp()
@@ -19,7 +99,5 @@ class PertipheralTests: XCTestCase {
     override func tearDown() {
         super.tearDown()
     }
-
-
     
 }
