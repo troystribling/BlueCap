ed0bc1873626b4a9520ac4c042e83ed852a8254d
Troy Stribling <troy.stribling@gmail.com>
2015-04-18 17:56:41 -0700

    started peripheral example

---
+++ Examples/Peripheral/Peripheral/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Peripheral
+//
+//  Created by Troy Stribling on 4/18/15.
+//  Copyright (c) 2015 gnos.us. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
