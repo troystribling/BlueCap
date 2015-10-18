da3b6871391f7045b647bbd7a788e1b158d9dd0a
troy Stribling <troy.stribling@gmail.com>
2015-10-18 13:37:55 -0700

    finished service advertisement uuid views

---
+++ BlueCap/Central/PeripheralAdvertisementsOverflowServicesViewController.swift
@@ -0,0 +1,71 @@
+//
+//  PeripheralAdvertisementsOverflowServicesViewController.swift
+//  BlueCap
+//
+//  Created by Troy Stribling on 10/18/15.
+//  Copyright © 2015 Troy Stribling. All rights reserved.
+//
+
+import UIKit
+import BlueCapKit
+
+class PeripheralAdvertisementsOverflowServicesViewController: UITableViewController {
+
+    weak var peripheral : Peripheral?
+    
+    struct MainStoryboard {
+        static let peripheralAdvertisementsOverflowServiceCell = "PeripheralAdvertisementsOverflowServiceCell"
+    }
+    
+    required init?(coder aDecoder:NSCoder)  {
+        super.init(coder:aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:"didBecomeActive", name:BlueCapNotification.didBecomeActive, object:nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:"didResignActive", name:BlueCapNotification.didResignActive, object:nil)
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func didResignActive() {
+        self.navigationController?.popToRootViewControllerAnimated(false)
+        Logger.debug()
+    }
+    
+    func didBecomeActive() {
+        Logger.debug()
+    }
+    
+    // UITableViewDataSource
+    override func numberOfSectionsInTableView(tableView:UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_:UITableView, numberOfRowsInSection section:Int) -> Int {
+        if let services = self.peripheral?.advertisedOverflowServiceUUIDs {
+            return services.count
+        } else {
+            return 0;
+        }
+    }
+    
+    override func tableView(tableView:UITableView, cellForRowAtIndexPath indexPath:NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(MainStoryboard.peripheralAdvertisementsOverflowServiceCell, forIndexPath:indexPath)
+        if let services = self.peripheral?.advertisedOverflowServiceUUIDs {
+            let service = services[indexPath.row]
+            cell.textLabel?.text = service.UUIDString
+        }
+        return cell
+    }
+    
+
+}
---
+++ BlueCap/Central/PeripheralAdvertisementsSolicitedServicesViewController.swift
@@ -0,0 +1,70 @@
+//
+//  PeripheralAdvertisementsSolicitedServicesViewController.swift
+//  BlueCap
+//
+//  Created by Troy Stribling on 10/18/15.
+//  Copyright © 2015 Troy Stribling. All rights reserved.
+//
+
+import UIKit
+import BlueCapKit
+
+class PeripheralAdvertisementsSolicitedServicesViewController: UITableViewController {
+
+    weak var peripheral : Peripheral?
+    
+    struct MainStoryboard {
+        static let peripheralAdvertisementsSolicitedServiceCell = "PeripheralAdvertisementsSolicitedServiceCell"
+    }
+    
+    required init?(coder aDecoder:NSCoder)  {
+        super.init(coder:aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:"didBecomeActive", name:BlueCapNotification.didBecomeActive, object:nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector:"didResignActive", name:BlueCapNotification.didResignActive, object:nil)
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func didResignActive() {
+        self.navigationController?.popToRootViewControllerAnimated(false)
+        Logger.debug()
+    }
+    
+    func didBecomeActive() {
+        Logger.debug()
+    }
+    
+    // UITableViewDataSource
+    override func numberOfSectionsInTableView(tableView:UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_:UITableView, numberOfRowsInSection section:Int) -> Int {
+        if let services = self.peripheral?.advertisedSolicitedServiceUUIDs {
+            return services.count
+        } else {
+            return 0;
+        }
+    }
+    
+    override func tableView(tableView:UITableView, cellForRowAtIndexPath indexPath:NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(MainStoryboard.peripheralAdvertisementsSolicitedServiceCell, forIndexPath:indexPath)
+        if let services = self.peripheral?.advertisedSolicitedServiceUUIDs {
+            let service = services[indexPath.row]
+            cell.textLabel?.text = service.UUIDString
+        }
+        return cell
+    }
+
+}
