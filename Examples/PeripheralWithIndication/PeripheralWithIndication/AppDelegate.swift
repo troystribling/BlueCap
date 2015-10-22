17a7b01ef6f24be27f9557e2a9d36765b363d871
troy Stribling <troy.stribling@gmail.com>
2015-10-22 07:54:01 -0700

    looking at indications

---
+++ Examples/PeripheralWithIndication/PeripheralWithIndication/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  PeripheralWithIndication
+//
+//  Created by Troy Stribling on 10/21/15.
+//  Copyright © 2015 Troy Stribling. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+    
+    func applicationWillResignActive(application: UIApplication) {
+    }
+    
+    func applicationDidEnterBackground(application: UIApplication) {
+    }
+    
+    func applicationWillEnterForeground(application: UIApplication) {
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+    }
+    
+    func applicationWillTerminate(application: UIApplication) {
+    }
+    
+    
+}
+
+
---
+++ Examples/PeripheralWithIndication/PeripheralWithIndicationTests/PeripheralWithIndicationTests.swift
@@ -0,0 +1,36 @@
+//
+//  PeripheralWithIndicationTests.swift
+//  PeripheralWithIndicationTests
+//
+//  Created by Troy Stribling on 10/21/15.
+//  Copyright © 2015 Troy Stribling. All rights reserved.
+//
+
+import XCTest
+@testable import PeripheralWithIndication
+
+class PeripheralWithIndicationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
