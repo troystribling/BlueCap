ac2343de20e570156d9a0d739441d246a0ac105a
troy Stribling <troy.stribling@gmail.com>
2016-10-06 07:49:18 -0700

    reworking scanning

---
+++ Examples/BlueCap/BlueCap/Utils/DiscoveredPeripheralStore.swift
@@ -0,0 +1,9 @@
+//
+//  DiscoveredPeripheralStore.swift
+//  BlueCap
+//
+//  Created by Troy Stribling on 10/5/16.
+//  Copyright © 2016 Troy Stribling. All rights reserved.
+//
+
+import Foundation
